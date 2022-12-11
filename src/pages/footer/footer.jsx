e0417ad910348fbe8cdd49ed7445210038a0014d
Aaryan6 <85422491+Aaryan6@users.noreply.github.com>
2022-12-11 17:40:09 +0530

    insert links in  footer icon

--- src/pages/footer/footer.jsx
+++ src/pages/footer/footer.jsx
@@ -8,16 +8,24 @@ const Footer = () => {
       <div className="footer-wrapper">
         <div className="icons">
           <span>
-            <FiGithub className="icon" />
+            <a href="https://github.com/Aaryan6" target="_blank">
+              <FiGithub className="icon" />
+            </a>
           </span>
           <span>
-            <FiLinkedin className="icon" />
+            <a href="https://www.linkedin.com/in/aaryanpatel6/" target="_blank">
+              <FiLinkedin className="icon" />
+            </a>
           </span>
           <span>
-            <FiInstagram className="icon" />
+            <a href="https://www.instagram.com/aaryanpatel2.0/" target="_blank">
+              <FiInstagram className="icon" />
+            </a>
           </span>
           <span>
-            <FiTwitter className="icon" />
+            <a href="https://twitter.com/aaryanpatel_06" target="_blank">
+              <FiTwitter className="icon" />
+            </a>
           </span>
         </div>
         <span className="subtitle">Design by Aaryan Patel.</span>
